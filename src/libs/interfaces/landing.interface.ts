645f8412962969b5e6a0b4583ecaa49932801d12
Jeremy-Camus-Varela-Aiquia <jeremy.camus.varela@gmail.com>
2024-02-25 16:56:54 -0300

    feat: services component now renders external data

--- src/libs/interfaces/landing.interface.ts
+++ src/libs/interfaces/landing.interface.ts
@@ -1,10 +1,26 @@
 export interface LandingPageData {
   title: string;
-  heroData: {
-    title: string;
-    subTitle: string;
-    primaryCta: string;
-    secondaryCta: string;
-    highlightedTitle?: string;
-  };
+  heroData: HeroData;
+  servicesData: ServicesData;
 }
+
+export interface HeroData {
+  title: string;
+  subTitle: string;
+  primaryCta: string;
+  secondaryCta: string;
+  highlightedTitle: string;
+}
+
+export interface ServicesData {
+  title: string;
+  services: Service[];
+}
+
+export interface Service {
+  title: string;
+  icon: Icon;
+  description: string;
+}
+
+export type Icon = "DevIcon" | "FileIcon" | "PlanetIcon" | "ConfigIcon";
