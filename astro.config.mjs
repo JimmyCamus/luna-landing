a0879387d41fbfc2c1062e73cdb1740be3dedf4a
Jeremy-Camus-Varela-Aiquia <jeremy.camus.varela@gmail.com>
2024-02-25 23:57:19 -0300

    feat: robots.txt have been added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
-import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: import.meta.env.DEV ? "http://localhost:4321" : "https://example.com",
+  integrations: [tailwind(), sitemap(), robotsTxt()]
 });
