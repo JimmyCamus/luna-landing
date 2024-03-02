b46cc227d63a9b2cf08ae71543f89d73ec16e0eb
Jeremy-Camus-Varela-Aiquia <jeremy.camus.varela@gmail.com>
2024-03-02 13:27:33 -0300

    chore: deploy url have been updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-import sitemap from "@astrojs/sitemap";
 
 import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  site: import.meta.env.DEV ? "http://localhost:4321" : "https://example.com",
-  integrations: [tailwind(), sitemap(), robotsTxt()]
+  site: import.meta.env.DEV
+    ? "http://localhost:4321"
+    : "https://luna-landing-rust.vercel.app/",
+  integrations: [tailwind(), sitemap(), robotsTxt()],
 });
